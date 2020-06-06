3a4ed30164c92022cc5df32fee5a7d3ef213de28
Dylan Landry <dylanglandry@gmail.com>
2020-06-06 18:30:53 -0400

    feat: add login resolver, express redis sessions
    Finished Ben Awad's typegraphql tutorial ep 3.

--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,59 @@ import { ApolloServer } from "apollo-server-express";
 import Express from "express";
 import { buildSchema } from "type-graphql";
 import { createConnection } from "typeorm";
+import session from "express-session";
+import connectRedis from "connect-redis";
+import cors from "cors";
 
 import { RegisterResolver } from "./modules/user/Register";
+import { LoginResolver } from "./modules/user/Login";
+import { MeResolver } from "./modules/user/Me";
+import { redis } from "./redis";
 
 (async function main() {
   await createConnection();
+
+  // Build schema.
   const schema = await buildSchema({
-    resolvers: [RegisterResolver],
+    resolvers: [RegisterResolver, LoginResolver, MeResolver],
   });
-  const apolloServer = new ApolloServer({ schema });
+
+  const apolloServer = new ApolloServer({
+    schema,
+		// Add express req to resolver context.
+    context: ({ req }) => ({ req }),
+  });
+
   const app = Express();
+
+  // Enable cors.
+  app.use(
+    cors({
+      credentials: true,
+      origin: "http://localhost:3000",
+    })
+  );
+
+  // Configure redis for storing sessions.
+  const RedisStore = connectRedis(session);
+  app.use(
+    session({
+      store: new RedisStore({
+        client: redis,
+      }),
+      name: "qid",
+      secret: "aslkdfjoiq12312",
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        maxAge: 1000 * 60 * 60 * 24 * 7 * 365, // 7 years
+      },
+    })
+  );
+
   apolloServer.applyMiddleware({ app });
+
   app.listen(3000, () => console.log("listening on http://localhost:3000"));
 })();
